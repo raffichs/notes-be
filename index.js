3f9d6240392c793bc5d417a8df0a87cf693207df
raffichaesa <140777811+raffichs@users.noreply.github.com>
2025-06-04 20:22:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import cors from "cors";
 import NoteRoute from "./routes/NoteRoute.js";
 
 const app = express();
+app.set("view engine", "ejs");
+
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => res.render("index"));
 app.use(NoteRoute);
 
-app.listen(8080, () => console.log("Server is running on port 8080"));
+app.listen(5000, () => console.log("Server connected"));
